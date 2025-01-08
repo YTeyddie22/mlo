cff26717b73be69033862e08a149ad54e499adce
YTeyddie22 <teyddie@gmail.com>
2025-01-08 17:34:52 +0300

    add extra packages

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
 
+app.use(morgan("combined"));
+app.use(express.json());
+
+//! Test middleware;
+
+app.use((req, res, next) => {
+    req.requstTime = new Date().toISOString();
+    next();
+});
 module.exports = app;
