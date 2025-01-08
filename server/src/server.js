fd323392102ab39497767937bcab955d5c65e184
YTeyddie22 <teyddie@gmail.com>
2025-01-08 14:25:31 +0300

    fix: use express function

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,12 @@
 const dotenv = require("dotenv");
-const http = require("http");
 const app = require("./app");
 
 dotenv.config({ path: ".env" });
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer(app);
-
 async function startServer() {
-    server.listen(PORT, () => {
+    app.listen(PORT, () => {
         console.log(`Listening to port ${PORT}`);
     });
 }
