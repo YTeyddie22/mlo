b5078cf96a12b1f8e5d8bcb48e570b778528ddf6
YTeyddie22 <teyddie@gmail.com>
2025-01-16 14:12:18 +0300

    provide info of the node process

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,34 @@
 const dotenv = require("dotenv");
 const app = require("./app");
 
+process.on("uncaughtException", (err) => {
+    console.log("UNCAUGHT EXCEPTION! SHUTTING DOWN");
+    console.log(err.name, err.message);
+    process.exit(1);
+});
 dotenv.config({ path: ".env" });
 
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
-    app.listen(PORT, () => {
+    const server = app.listen(PORT, () => {
         console.log(`Listening to port ${PORT}`);
     });
 }
 
 startServer();
+
+process.on("unhandledRejection", (err) => {
+    console.log("UNHANDLED REJECTION! SHUTTING DOWN");
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+process.on("SIGTERM", (err) => {
+    console.log("SIGTERM RECIEVED! SHUTTING DOWN GRACEFULLY");
+    server.close(() => {
+        console.log("PROCESS TERMINATED");
+    });
+});
