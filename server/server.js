8c87f981639e98a54ab065fde73f14a996410fc5
YTeyddie22 <teyddie@gmail.com>
2025-02-25 13:50:49 +0300

    fix: handle unhandle rejection error

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv");
 const http = require("http");
 const app = require("./app");
-const Logger = require("./src/packages/common/logger")
+const Logger = require("./src/packages/common/logger");
 
 process.on("uncaughtException", (err) => {
-    Logger.error("---Uncaught Exception--->", true, err)
+    Logger.error("---Uncaught Exception--->", true, err);
     console.log("UNCAUGHT EXCEPTION! SHUTTING DOWN");
     console.log(err.name, err.message);
     process.exit(1);
@@ -24,7 +24,7 @@ async function startServer() {
 startServer();
 
 process.on("unhandledRejection", (err) => {
-    Logger.error('---Uncaught Exception--->', true, err);
+    Logger.error("---Uncaught Rejection--->", true, err);
     console.log("UNHANDLED REJECTION! SHUTTING DOWN");
     console.log(err.name, err.message);
     server.close(() => {
@@ -33,7 +33,7 @@ process.on("unhandledRejection", (err) => {
 });
 
 process.on("SIGTERM", (err) => {
-    Logger.error('---SIGTERM issue--->', true, err);
+    Logger.error("---SIGTERM issue--->", true, err);
     console.log("SIGTERM RECIEVED! SHUTTING DOWN GRACEFULLY");
     server.close(() => {
         console.log("PROCESS TERMINATED");
